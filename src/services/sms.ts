06a00d0e754d203e860d0a99091bb5a8c2b9a923
AmirSolt <soltamir194@gmail.com>
2023-12-11 06:38:38 -0500

    fixing bug

--- src/services/sms.ts
+++ src/services/sms.ts
@@ -8,16 +8,17 @@ const client = new Twilio(accountSid, authToken);
 export const smsRouter = express.Router()
 
 smsRouter.post('/inbound', (req: Request, res: Response) => {
+    const message = req.body.Body;
     const twimlResponse = new twiml.MessagingResponse();
     console.log("--- recieved sms")
-    console.log(`-- body: ${req.body}`)
-    sendSMS(req.body)
+    console.log(`-- body: ${message}`)
+    sendSMS(message).catch(error => console.error("Error sending SMS:", error));
     res.type('text/xml').send(twimlResponse.toString());
 });
 
 smsRouter.get('/outbound', (req: Request, res: Response) => {
     console.log("--- sms sent")
-    sendSMS("outbound")
+    sendSMS("Outbound").catch(error => console.error("Error sending SMS:", error));
     res.redirect('/');
 });
 
@@ -30,19 +31,19 @@ smsRouter.post('/inbound/fail', (req: Request, res: Response) => {
 
 
 
-function sendSMS(text:string) {
-
+async function sendSMS(text:string): Promise<void> {
     if (accountSid && authToken && twilioNumber) {
-        client.messages
-            .create({
+        try {
+            const message = await client.messages.create({
                 from: twilioNumber,
                 to: "+16475807443",
                 body: text,
-            })
-            .then((message) => console.log(message.sid));
+            });
+            console.log("SMS sent with SID:", message.sid);
+        } catch (error) {
+            console.error("Failed to send SMS:", error);
+        }
     } else {
-        console.error(
-            "You are missing one of the variables you need to send a message"
-        );
+        console.error("You are missing one of the variables needed to send a message");
     }
 }
